8334dec7259ecc9216022abd4999176ad08b5577
Ellie <jssoyeon@gmail.com>
2021-05-24 16:00:16 +0900

    create MyProject

---
+++ java_project/MyProject/src/Calculator.java
@@ -0,0 +1,16 @@
+
+public class Calculator {
+	int addNum(int x, int y) {
+		return x+y;
+	}
+	int subNum(int x, int y) {
+		return x-y;
+	}
+	int mulNum(int x, int y){
+		return x*y;
+	}
+	float divNum(int x, int y) {
+		return x/y;
+	}
+	
+}
