954ecac9a44e4644217d1c4721f3bae16d67377c
Ellie <jssoyeon@gmail.com>
2021-05-27 22:24:16 +0900

    ArrayQuiz recheck

--- Quiz/src/ch4/ArrayQuiz1.java
+++ Quiz/src/ch4/ArrayQuiz1.java
@@ -1,7 +1,7 @@
 package ch4;
 import java.util.Scanner;
 public class ArrayQuiz1 {
-	public static int miniValue(int[] arr) {
+	public static int minValue(int[] arr) {
 		int min = arr[0];
 		for(int i = 1; i<arr.length;i++) {
 			if(arr[i]<min) {
@@ -22,18 +22,14 @@ public class ArrayQuiz1 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int i = 0;
-		int[] arr = new int[10];  //사용자로 부터 입력받은 값을 저장하는 배열.
-		for(;;) {
-			System.out.println("숫자를 입력해주세요. (그만 입력하실려면 0, 최대10개)");
-			int num = sc.nextInt();
-			if(num ==0) {
-				break;
-			}
-			arr[i++]= num;
+		int []arr = new int[5];//사용자에게 받을 숫자 배열
+		for(int i=0;i<arr.length;i++) {
+			System.out.println("숫자를 입력하세요.");
+			arr[i]= sc.nextInt();
 		}
-		System.out.println("입력하신 값 중에 최대값은 ? "+ArrayQuiz1.maxValue(arr));
-		System.out.println("입력하신 값 중에 최소값은? "+ArrayQuiz1.miniValue(arr));
+		System.out.println("입력하신 숫자중에 최대값 : "+ maxValue(arr));
+		System.out.println("입력하신 숫자중에 최소값 : "+minValue(arr));
+		
 	}
 
 }
--- Quiz/src/ch4/ArrayQuiz3.java
+++ Quiz/src/ch4/ArrayQuiz3.java
@@ -1,20 +1,20 @@
 package ch4;
 public class ArrayQuiz3 {
-	static void change(int[][] arr) {
+	static void changeDown(int[][] arr) {
 
 		int[]tmp = arr[arr.length-1];
 		for(int i = arr.length-1;i>0;i--) {
 			arr[i] = arr[i-1];
 		}
 		arr[0]= tmp;
-
-		for(int i =0; i<arr.length;i++) {
-			for(int j=0;j <arr[i].length;j++) {
-				System.out.print(arr[i][j]);
-			}
-			System.out.println();
+	}
+	static void changeUp(int [][] arr) {
+		int[] tmp = arr[arr.length-1];
+		for(int i=0 ; i<arr.length-1;i++) {
+			tmp = arr[i];
+			arr[i] = arr[i+1];
+			arr[i+1]=tmp;
 		}
-		System.out.println();
 	}
 	public static void main(String[] args) {
 		int[][] arr = {
@@ -24,6 +24,22 @@ public class ArrayQuiz3 {
 				{2,3,4},
 				{5,2,3}
 		};
-		change(arr);
+		//한칸씩 내리기
+		changeDown(arr);
+		for( int[] tmp: arr) {
+			for( int e : tmp) {
+				System.out.print(e+" ");
+			}System.out.println();
+		}
+		System.out.println("=================");
+		//한칸씩 올리기
+		changeUp(arr);
+		changeUp(arr);
+		for(int[] tmp : arr) {
+			for(int e : tmp) {
+				System.out.print(e+" ");
+			}System.out.println();
+		}
+		
 	}
 }
