c81b2d1efb9d107f4365e7b3d8d83c31900e70c1
Ellie <jssoyeon@gmail.com>
2021-05-27 15:51:59 +0900

    constructor

--- java_project/MyProject/src/ex/Car.java
+++ java_project/MyProject/src/ex/Car.java
@@ -1,5 +1,38 @@
 package ex;
-
+//상속. 생성자 만들기 실습
 public class Car {
-
+	int gasolinegauge;
+	Car(int gas){
+		this.gasolinegauge = gas;
+	}
 }
+class HybridCar extends Car {
+	int electronicGauge;
+	HybridCar(int gas) {
+		super(gas);
+	}
+	HybridCar(int gas, int eg) {
+		super(gas);
+		electronicGauge =eg;
+	}
+}
+class HybridWaterCar extends HybridCar {
+	int waterGauge;
+	HybridWaterCar(int gas) {
+		super(gas);
+	}
+	HybridWaterCar(int gas,int eg) {
+		super(gas, eg);
+	}
+	HybridWaterCar(int gas,int eg, int wg) {
+		super(gas, eg);
+		waterGauge =wg;
+	}
+	
+	public void showCurrentGauge() {
+		System.out.println("잔여 가솔린 : " + gasolinegauge);
+		System.out.println("잔여 가솔린 : " + electronicGauge);
+		System.out.println("잔여 가솔린 : " + waterGauge);
+	}
+}
+
