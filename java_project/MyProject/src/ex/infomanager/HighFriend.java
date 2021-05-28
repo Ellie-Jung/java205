a275d98852b2e00fd9a5143e1207fddd187d1424
Ellie <jssoyeon@gmail.com>
2021-05-28 17:02:03 +0900

    FriendInfor manager

---
+++ java_project/MyProject/src/ex/infomanager/HighFriend.java
@@ -0,0 +1,26 @@
+package ex.infomanager;
+
+public class HighFriend extends Friend {
+	
+	//고교친구는 직장정보를 추가적으로 가진다.
+	String work;
+	
+	public HighFriend(String name, String phonNumber, String address,String work) {
+		super(name, phonNumber, address);
+		this.work = work;
+	}
+
+	@Override
+	public void showData() {
+		super.showData();
+		System.out.println("직   업 : "+ work);
+	}
+
+	@Override
+	public void showBasicInfo() {
+		System.out.println("이름 : "+name);
+		System.out.println("전화 : "+phonNumber);
+	}
+
+	
+}
