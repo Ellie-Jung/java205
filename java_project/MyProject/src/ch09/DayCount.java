1e2881d280376471d7c702ac4f85ddc26164d9a3
Ellie <jssoyeon@gmail.com>
2021-05-31 19:33:00 +0900

    Update DayCount.java

--- java_project/MyProject/src/ch09/DayCount.java
+++ java_project/MyProject/src/ch09/DayCount.java
@@ -1,17 +1,13 @@
 package ch09;
 
-import java.time.LocalDate;
+import java.time.*;
 import java.time.temporal.ChronoUnit;
-import java.util.Calendar;
 import java.util.Scanner;
 
 public class DayCount {
 
 	public static void main(String[] args) {
-		
-		Calendar date1= Calendar.getInstance(); //사용자에게 입력받을값
-		Calendar date2= Calendar.getInstance();//오늘날짜 저장
-		
+	
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("태어난 년도를 입력하세요");
 		int year = scanner.nextInt();
@@ -28,18 +24,7 @@ public class DayCount {
 		System.out.println("today : "+now );
 		System.out.println("days : "+days );
 		
-		 
-//		 date1.set(year,month-1,date);
-//		 date2.set(Calendar.getInstance().YEAR, Calendar.getInstance().MONTH,Calendar.getInstance().DATE);
-//		 
-//		 SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-//		 System.out.println("birth day = "+df.format(new Date(date1.getTimeInMillis())));
-//		 System.out.println("today = "+df.format(new Date(date2.getTimeInMillis())));
-//		 
-//		 long difference =
-//				 (date2.getTimeInMillis() - date1.getTimeInMillis())/1000;
-//				 System.out.println(difference/(24*60*60)
-//				 +" days"); // 1 = 24 * 60 * 60
+	
 	}
 
 }
