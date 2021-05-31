9fb302fea9e5a7202b4f37c0f9e6ecec5725094f
Ellie <jssoyeon@gmail.com>
2021-05-31 17:33:12 +0900

    ch08 update

--- java_project/MyProject/src/ch08/CalculatorImpl.java
+++ java_project/MyProject/src/ch08/CalculatorImpl.java
@@ -1,8 +1,11 @@
 package ch08;
 
-public class CalculatorImpl extends CalculatorAbs implements Calculator {
+public class CalculatorImpl implements Calculator {
 
-
+	@Override
+	public long add(long n1, long n2) {
+		return n1+n2;
+	}
 	@Override
 	public long substract(long n1, long n2) {
 		return n1-n2;
--- java_project/MyProject/src/ch08/CalculatorTest.java
+++ java_project/MyProject/src/ch08/CalculatorTest.java
@@ -5,14 +5,11 @@ public class CalculatorTest {
 	public static void main(String[] args) {
 
 		Calculator calcI = new CalculatorImpl();
-		CalculatorImpl calcImpl = new CalculatorImpl();
-		CalculatorAbs calculatorAbs = new CalculatorImpl();
-		
 		
 		System.out.println(calcI.add(3, 5));
 		System.out.println(calcI.substract(7, 2));
-		System.out.println(calcImpl.divide(10, 3));
-		System.out.println(calculatorAbs.multiply(6, 7));
+		System.out.println(calcI.divide(10, 3));
+		System.out.println(calcI.multiply(6, 7));
 	}
 
 }
