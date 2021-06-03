16ff7f7caec1939656565cf9cff88abb8746e1f3
Ellie <jssoyeon@gmail.com>
2021-06-03 19:44:40 +0900

    File Clone Thread

--- java_project/MyProject/src/ch13/CloneMain.java
+++ java_project/MyProject/src/ch13/CloneMain.java
@@ -11,11 +11,17 @@ public class CloneMain {
 		Scanner sc = new Scanner(System.in);
 		CloneMain c = new CloneMain();
 		
-		System.out.println("복사할 대상의 파일 경로를 입력하세요.");
+		System.out.println("복사할 대상의 파일 경로를 포함한 파일이름을 입력하세요.");
 		String path = sc.nextLine();
-		System.out.println("복사해 올 위치 경로를 입력하세요");
+		File f = new File(path);
+		if(!f.exists()) {
+			System.out.println("파일이 없습니다.");
+		}
+			
+		System.out.println("복사해 올 위치 경로를 포함한 복사될 파일이름을 입력하세요.");
 		String clonePath = sc.nextLine();
-
+		File f2= new File(clonePath);
+		
 		
 		//데이터 복사하는 스레드 생성
 		DataClone t = new DataClone(path, clonePath);
