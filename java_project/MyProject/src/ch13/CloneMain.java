f85431af239bf4ceedbfa88e4d3f90e87c27a5b6
Ellie <jssoyeon@gmail.com>
2021-06-03 19:27:30 +0900

    File Clone Thread

--- java_project/MyProject/src/ch13/CloneMain.java
+++ java_project/MyProject/src/ch13/CloneMain.java
@@ -10,14 +10,11 @@ public class CloneMain {
 		//복사할 대상파일의 경로와 복사해올 위치 경로를 받는 메인스레드는 멈춤없이 실행.
 		Scanner sc = new Scanner(System.in);
 		CloneMain c = new CloneMain();
-
-		String path;
-		String clonePath;
 		
 		System.out.println("복사할 대상의 파일 경로를 입력하세요.");
-		path = sc.nextLine();
-		System.out.println("복사해올 위치경로를 입력하세요");
-		clonePath = sc.nextLine();
+		String path = sc.nextLine();
+		System.out.println("복사해 올 위치 경로를 입력하세요");
+		String clonePath = sc.nextLine();
 
 		
 		//데이터 복사하는 스레드 생성
--- java_project/MyProject/src/ch13/DataClone.java
+++ java_project/MyProject/src/ch13/DataClone.java
@@ -3,10 +3,11 @@ package ch13;
 import java.io.*;
 
 public class DataClone extends Thread {
+	//데이터의 복사를 처리하는 스레드 
+
 	private String path;
 	private String clonePath;
 
-	//데이터의 복사를 처리하는 스레드 
 	DataClone(String path, String clonePath) {
 		this.path = path;
 		this.clonePath = clonePath;
@@ -19,11 +20,11 @@ public class DataClone extends Thread {
 		try {
 			//파일의 데이터 읽어올 스트림 생성
 			InputStream in = new BufferedInputStream(new FileInputStream(path));
-			
 			//파일을 쓰기위한 스트림 생성
 			OutputStream out = new BufferedOutputStream(new FileOutputStream(clonePath));
-		
-			int copyByte=0;
+			
+			int copyByte=0; //데이터 크기체크
+			
 			//파일 읽기
 			while (true) {
 				
