5696ecd81f442ae6f06e252f7090c05d2fe5889b
Ellie <jssoyeon@gmail.com>
2021-06-03 22:18:34 +0900

    Update GetIdCheck.java

--- java_project/MyProject/src/ch10/GetIdCheck.java
+++ java_project/MyProject/src/ch10/GetIdCheck.java
@@ -5,26 +5,30 @@ import java.util.Scanner;
 public class GetIdCheck {
 	public static void main(String[] args) {
 
-
 		Scanner sc = new Scanner(System.in);
-		System.out.print("아이디를 입력하세요 >>");
-		String userId = sc.nextLine();
+		System.out.println("아이디를 입력해주세요 (영문 또는 숫자만 허용)");
+		String id = sc.nextLine();
+		id = id.trim();
 
-		userId = userId.trim().toUpperCase();
-		char[] ch = userId.toCharArray();
-		char chk = 0;
-		for(int i = 0; i<userId.length();i++) {
-			 chk=ch [i];
-		}
 		try {
-			if(userId.length()==0 || !((chk >=65&& chk<=90 ) ||(chk>=48 &&chk<=57))) {
-				throw new BadIdInputException("잘못입력하셨습니다.");
-			}
-		}catch (BadIdInputException e) {
+			if(!(id.length()==0)){
+				char chk = 0;
+				for(int i =0; i<id.length(); i++) {
+					chk = id.toLowerCase().charAt(i);
+				}
+				if(!(chk>='a'&&chk<='z'  || chk>='A'&&chk<='Z'|| chk>='0' && chk<='9')){
+					throw new BadIdInputException("아이디는 숫자나 영문만 가능합니다.");
+				}else {
+					System.out.println("아이디는 "+ id+"입니다.");
+				}
+			}else 
+				System.out.println("빈칸을 입력하셨습니다.");
 			
+		} catch (BadIdInputException e) {
 			System.out.println(e.getMessage());
 		}
 
 	}
+
 }
 
