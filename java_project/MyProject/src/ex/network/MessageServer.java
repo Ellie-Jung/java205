8338d3f1921b747b176cc50c68fec9d51f89c070
Ellie <jssoyeon@gmail.com>
2021-06-04 16:24:39 +0900

    Networking

---
+++ java_project/MyProject/src/ex/network/MessageServer.java
@@ -0,0 +1,64 @@
+package ex.network;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class MessageServer {
+
+	public static void main(String[] args) {
+
+		DataInputStream din = null;
+		DataOutputStream dout = null;
+		ServerSocket ss = null;
+		Socket s =null;
+		
+
+		try {
+			ss = new ServerSocket(8888);
+			
+			System.out.println("서버실행");
+			System.out.println("-------------------------------");
+			
+			s = ss.accept();
+			System.out.println("client 접속");
+			System.out.println("-------------------------------");
+
+			din = new DataInputStream(s.getInputStream());
+			dout = new DataOutputStream(s.getOutputStream());
+
+			Scanner sc = new Scanner(System.in);
+
+			String str1 = null; //받는 메세지
+			String str2 = "";//보내는 메세지
+
+			while(!str2.equals("exit")) {
+
+				str1 = din.readUTF();
+				System.out.println("Client Message :"+str1);
+
+				str2 = sc.nextLine();
+				dout.writeUTF(str2);
+				dout.flush();
+			}
+
+
+
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			
+			// 닫는거 따로 묶어서 만들어놓고 사용 (편리)
+			Util.close(din);
+			Util.close(dout);
+			Util.close(s);
+			Util.close(ss);
+			
+
+		}
+	}
+}
