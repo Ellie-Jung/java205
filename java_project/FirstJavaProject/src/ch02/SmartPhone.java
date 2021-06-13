bc9fd2b940fffc4e414a25ee1750fb341fe1f377
Ellie <jssoyeon@gmail.com>
2021-06-13 22:24:03 +0900

    ch02 create

--- java_project/FirstJavaProject/src/ch02/SmartPhone.java
+++ java_project/FirstJavaProject/src/ch02/SmartPhone.java
@@ -18,21 +18,63 @@ public class SmartPhone {
 	}
 	void editInfo(){ //배열 수정
 		System.out.print("수정하실 연락처의 이름을 적어주세요 >>");
+		String name = sc.nextLine().trim();
+		for (int i = 0; i<con.length;i++) {
+			if(name.equals(con[i].getName())){
+				System.out.println("어떤 정보를 수정하시겠습니까?");
+				System.out.print("1. 이름 2.전화번호 3.이메일 4.주소 5.생일 6.그룹 7.전체>>");
+				String num = sc.nextLine().trim();
+				System.out.println("변경하실 내용을 입력해주세요.");
+				switch (num) {
+				case "1":
+					con[i].setName(sc.nextLine());
+					break;
+				case "2":
+					con[i].setPhoneNum(sc.nextLine());
+					break;
+				case "3":
+					con[i].setEmail(sc.nextLine());
+					break;
+				case "4":
+					con[i].setAddress(sc.nextLine());
+					break;
+				case "5":
+					con[i].setBirthday(sc.nextLine());
+					break;
+				case "6":
+					con[i].setGroup(sc.nextLine());
+					break;
+				case "7":
+					System.out.println("이름을 입력하세요");
+					con[i].setName(sc.nextLine());
+					System.out.println("전화번호를 입력하세요");
+					con[i].setPhoneNum(sc.nextLine());
+					System.out.println("메일을 입력하세요");
+					con[i].setEmail(sc.nextLine());
+					System.out.println("주소를 입력하세요");
+					con[i].setAddress(sc.nextLine());
+					System.out.println("생일을 입력하세요");
+					con[i].setBirthday(sc.nextLine());
+					System.out.println("그룹을 입력하세요");
+					con[i].setGroup(sc.nextLine());
+					break;
+				}
+			}
+		} System.out.println("수정완료");
 
 	}
+	
 	void deleteInfo() {//배열의 요소를 삭제
 		System.out.print("삭제하실 연락처의 이름을 적어주세요 >>");
 		String name = sc.nextLine().trim();
-		for(int i=0; i<con.length;i++) {
+		for(int i =0; i<con.length;i++) {
 			if(name.equals(con[i].getName())){
-				for(int j=i;j<con.length-1;j++) {
-					Contact temp = con[i];
-					con[i] =con[i+1];
-					con[i+1] = new Contact();
-				}
+				System.arraycopy(con, i+1, con, i, con.length-i-1);
+				con[con.length-1] = new Contact();
 			}
 		}System.out.println(name+" 삭제완료.");
 	}
+	
 	void printInfo() {
 		for( Contact e: con) {
 			System.out.println("이름 : "+e.getName());
@@ -51,7 +93,7 @@ public class SmartPhone {
 
 
 		SmartPhone smartPhone = new SmartPhone();
-	
+
 		for(int i =0 ; i< smartPhone.con.length;i++) {
 
 			System.out.println("["+(i+1)+"]");
@@ -79,6 +121,8 @@ public class SmartPhone {
 		smartPhone.deleteInfo(); // 배열의 요소를 삭제
 		smartPhone.printInfo(); // 삭제 확인하기
 
+		smartPhone.editInfo(); //배열의 요소를 수정
+		smartPhone.printInfo(); // 수정 확인
 	}
 
 }
