6de94a92bc24276f4434f16524846ca72e190828
Ellie <jssoyeon@gmail.com>
2021-06-15 17:55:11 +0900

    Update ddlEx_20210615.sql

--- SQL/ddlEx_20210615.sql
+++ SQL/ddlEx_20210615.sql
@@ -2,15 +2,15 @@
 --DDL EX
 
 create table phoneInfo_basic ( idx NUMBER(6),
-                         fr_name VARCHAR2(20) NOT NULL,
-                         fr_phonenumber VARCHAR2(20) NOT NULL,
-                         fr_email VARCHAR2(20),
-                         fr_address VARCHAR2(20),
-                         fr_regdate DATE DEFAULT SYSDATE,
+                              fr_name VARCHAR2(20) NOT NULL,
+                              fr_phonenumber VARCHAR2(20) NOT NULL,
+                              fr_email VARCHAR2(20),
+                              fr_address VARCHAR2(20),
+                              fr_regdate DATE DEFAULT SYSDATE,
                          
-                         constraint phoneInfo_basic_idx_PK PRIMARY KEY(idx)
+                              constraint phoneInfo_basic_idx_PK PRIMARY KEY(idx)
                          
-                         );
+                              );
                   
 create table phoneInfo_univ ( idx NUMBER(6),
                               fr_u_major VARCHAR2(20)DEFAULT 'N' NOT NULL , 
