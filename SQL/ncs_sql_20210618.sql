c507bb52d1fec3a313e2457139dc335a8d5373c3
Ellie <jssoyeon@gmail.com>
2021-06-19 01:29:08 +0900

    Update ncs_sql_20210618.sql

--- SQL/ncs_sql_20210618.sql
+++ SQL/ncs_sql_20210618.sql
@@ -27,29 +27,46 @@ create table Contact ( pIdx  NUMBER(10),
                        meetingname VARCHAR2(20),
                        nickname VARCHAR2(20),
                        
-                       
-                       
+                                              
                        CONSTRAINT CONTACT_pIdx_PK PRIMARY KEY(pIdx),
                        CONSTRAINT CONTACT_friendtype_chk check(friendtype='univ' and friendtype='com' and friendtype='cafe')
-                       
-                       
+                                              
                        );
 
 
-
-
+select * from test;
 --2. DEPT 테이블에 데이터를 삽입하는 SQL을 작성하시오. 입력 데이터는 임의로 작성하시오.
---3. DEPT 테이블에 위에서 삽입한 데이터의 dname, loc 데이터를 변경하는 SQL을 작성하시오.
---   입력 데이터는 임의로 작성하시오.
+insert into dept values (50, 'DEV', 'SEOUL');
+
+--3. DEPT 테이블에 위에서 삽입한 데이터의 dname, loc 데이터를 변경하는 SQL을 작성하시오. 입력 데이터는 임의로 작성하시오.
+update dept set dname = 'DESIGN' where deptno = 50;
+
 --4. DEPT 테이블에 위에서 삽입한 데이터를 deptno 컬럼의 값을 기반으로 삭제하는 SQL을 작성하시오.
+delete from dept where deptno = 50;
+
 --5. 사용자가 보유한 테이블 목록을 확인하는 SQL문을 작성하시오.
+select * from user_tables;
+
 --6. EMP 테이블의 구조를 확인하는 SQL을 작성하시오.
+select * from emp;
+
 --7. 사용자가 정의한 제약조건들을 확인하는 SQL문을 작성하시오.
---
---
+select * from user_constraints;
+DESC user_constraints; 
+
 --#2 아래 요구사항에 맞도록 고급 SQL 문을 작성하시오.
---
+
 --1. EMP 테이블의 ename 컬럼에 인덱스를 생성하는 SQL을 작성하시오. 인덱스의 이름은 emp_index
---2. EMP 테이블과 DEPT 테이블을 조인하는 SQL을 기반으로 view 객체를 생성하는 SQL을 작성하시오.
---   view 의 이름은 emp_view 로 하시오.
+create index emp_index on emp(ename);
+
+--2. EMP 테이블과 DEPT 테이블을 조인하는 SQL을 기반으로 view 객체를 생성하는 SQL을 작성하시오. view 의 이름은 emp_view 로 하시오.
+create or replace view emp_view
+as
+select * from emp natural join dept
+order by empno;
+
+select * from emp_view;
+
 --3. EMP 테이블에서 모든 사원의 부서번호를 이름이 'SCOTT'인 사원의 부서번호로 변경하는 SQL을 작성하시오.
+update emp set deptno =(select deptno from emp where ename ='SCOTT'); 
+  
