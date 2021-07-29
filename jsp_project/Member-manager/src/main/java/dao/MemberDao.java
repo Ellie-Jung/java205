dd850852a283affb58050eb69f2aa44af1c1d374
Ellie <jssoyeon@gmail.com>
2021-07-29 17:40:11 +0900

    member manager id chk

--- jsp_project/Member-manager/src/main/java/dao/MemberDao.java
+++ jsp_project/Member-manager/src/main/java/dao/MemberDao.java
@@ -138,6 +138,35 @@ public class MemberDao {
 	}
 	
 	
+	// ID 중복 여부 확인을 위한 id 값으로 검색 -> 개수반환
+	public int selectById(Connection conn, String memberId) throws SQLException {
+		
+		int cnt= 0;
+		
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		String sql = "select count(*) from member where memberid=?";
+		try {
+			pstmt = conn.prepareStatement(sql);// 예외던지기
+			pstmt.setString(1, memberId);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				cnt = rs.getInt(1);
+			}
+		
+		}finally {
+			JdbcUtil.close(rs);
+			JdbcUtil.close(pstmt);
+		}
+		 		
+				
+		return cnt;
+	}
+	
+	
 	
 	
 	
