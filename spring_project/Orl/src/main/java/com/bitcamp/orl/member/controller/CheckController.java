96e408302851ac8ca164ddc2e1fc29ae67abc3e0
Ellie <jssoyeon@gmail.com>
2021-09-10 18:35:34 +0900

    member page 유효성검사

--- spring_project/Orl/src/main/java/com/bitcamp/orl/member/controller/CheckController.java
+++ spring_project/Orl/src/main/java/com/bitcamp/orl/member/controller/CheckController.java
@@ -1,12 +1,10 @@
 package com.bitcamp.orl.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,24 +13,31 @@ import com.bitcamp.orl.member.service.CheckService;
 @RestController
 public class CheckController {
 
-	@Autowired
-	private CheckService service;
-	
-	@GetMapping("/member/idCheck")
-	@CrossOrigin
-	public String idCheck(
-			@RequestParam("mid") String uid
-			) {
-		return service.idCheck(uid);
-	}
-	
-	
-	  @GetMapping("/member/nickNameCheck")
-	  @CrossOrigin 
-	  public String nickNameCheck(
-			  	@RequestParam("nickname") String nickname ) { 
-		  
-		  return service.nickNameCheck(nickname); }
-	 
-	
+    @Autowired
+    private CheckService service;
+
+    @GetMapping("/member/idCheck")
+    @CrossOrigin
+    public String idCheck(
+            @RequestParam("mid") String uid
+    ) {
+        return service.idCheck(uid);
+    }
+
+
+    @GetMapping("/member/nickNameCheck")
+    @CrossOrigin
+    public String nickNameCheck(
+            @RequestParam("nickname") String nickname) {
+
+        return service.nickNameCheck(nickname);
+    }
+
+    @GetMapping("/member/emailCheck")
+    @CrossOrigin
+    public String emailCheck(
+            @RequestParam("email") String email) {
+        return service.emailCheck(email);
+    }
+
 }
